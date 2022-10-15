3a0c787a24eca75da740dcd433b09e26cdcc8af2
svtart <promixmaxads@gmail.com>
2022-10-15 20:30:58 +0300

    Final fixed version 2.0

--- src/main/java/web/controller/CarController.java
+++ src/main/java/web/controller/CarController.java
@@ -4,13 +4,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import web.model.Car;
 import web.service.CarServiceImpl;
 
-import java.util.ArrayList;
 import java.util.List;
 
 @Controller
